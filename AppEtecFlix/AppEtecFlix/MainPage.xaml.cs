ea8cd50109829d34a766c5657e3e113e2f9d65d2
Ana Clara <89492133+anacll-dev@users.noreply.github.com>
2023-05-08 11:18:50 -0300

    Adicionar arquivos de projeto.

---
+++ AppEtecFlix/AppEtecFlix/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppEtecFlix.Categorias;
+
+namespace AppEtecFlix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Aventura());
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
